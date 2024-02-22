3d9f722951eb2cd3122dcc0d5efcb2c7a1ac785a
Raffaleite <Rafaelleite.comp@gmail.com>
2024-02-22 18:30:59 -0300

    commit-2

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode(){
    const img = document.querySelector(".profile img")
 
    if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/profile2.jpg')
+    img.setAttribute('src', './assets/profile2.png')
     img.setAttribute('alt', "imagem de fundo branco")
    } else {
-    img.setAttribute('src', './assets/profile.jpg')
+    img.setAttribute('src', './assets/profile.png')
     img.setAttribute('alt', "imagem de fundo preto")
    }
 
